c1fcc0af049d2b507b458ad1a0e39bccc3aaa82a
Nire Bryce <nire@computernope.net>
2026-01-01 19:19:07 -0500

    made read_lines something more managable than the example given to me

--- src/util/inputs.rs
+++ src/util/inputs.rs
@@ -1,21 +1,22 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead};
 use std::path::Path;
 
 /// Reads a file and returns an iterator containing it's lines.
 /// Each line wraps with Result to handle I/O errors.
-pub fn read_lines<P>(path: P) -> io::Result<impl Iterator<Item = io::Result<String>>> 
-where P: AsRef<Path>, // allows us to accept any type that implements AsRef<Path>, 
+// pub fn read_lines<P>(path: P) -> io::Result<impl Iterator<Item = io::Result<String>>> 
+// where P: AsRef<Path>, // allows us to accept any type that implements AsRef<Path>, 
                       // look this up later because I don't know it yet
-{
+pub fn read_lines(source_path: &str) -> io::Result<Vec<String>> {
+    let path = Path::new(source_path);
     let file = File::open(path?);
 
     // Wrap the file in a BufReader for faster line-by-line reading
     //    without buffering, each read syscall would be expensive.
-    let reader = BufReader::new(file);
+    let reader = io::BufReader::new(file);
 
     // return an iterator where each item is Result<String>
-    Ok(reader.lines())
+    reader.lines().collect()
 }
 
 
